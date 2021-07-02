d109824b2adb40a70f4c0aaa1c3d814b5cb6614b
zepholus <joansalograu@gmail.com>
2021-07-02 09:36:02 +0200

    second navbar test

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,83 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="app">
+    <v-toolbar flat>
+      <v-toolbar-title class="headline">
+        <span>Testing...</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-toolbar>
+    <v-layout fill-height>
+      <v-navigation-drawer
+          style="z-index:2"
+          dark
+          clipped
+          stateless
+          v-model="leftMenu"
+      >
+        <v-container ma-0 pa-0>
+          <v-toolbar flat>
+            <span>Some helper toolbar</span>
+          </v-toolbar>
+        </v-container>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-action>
+              <v-icon @click="rightMenu = !rightMenu">android</v-icon>
+            </v-list-tile-action>
+            <v-list-tile-content>
+              <v-list-tile-title>Task Menu</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-navigation-drawer id="nav2"
+                           dark
+                           style="z-index:1"
+                           v-model="rightMenu"
+                           stateless>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-content>
+              <v-list-tile-title>Testing right nav</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+        </v-list>
+      </v-navigation-drawer>
+      <v-main>
+        <router-view></router-view>
+      </v-main>
+    </v-layout>
+
+  </v-app>
 </template>
 
+<script>
+import Navbar from "@/components/Navbar";
+export default {
+  components: {Navbar},
+  data () {
+    return {
+      leftMenu: true,
+      rightMenu: false,
+    }
+  },
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+
+html {
+  overflow: hidden !important;
+  scrollbar-width: none;
+  -ms-overflow-style: none;
 }
 
-#nav {
-  padding: 30px;
+html::-webkit-scrollbar {
+  width: 0;
+  height: 0;
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+#app{
+  background-color: aqua
 }
 </style>
