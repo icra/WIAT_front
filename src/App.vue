28005cb786d010dd5ca38c06ec28d0f8bc6c1a73
zepholus <joansalograu@gmail.com>
2021-07-07 18:20:58 +0200

    assessments collapsed

--- src/App.vue
+++ src/App.vue
@@ -15,13 +15,14 @@
     </v-toolbar>
     <v-layout fill-height>
 
-      <!-- Main sidebar -->
+      <!-- Main sidebar (first)-->
       <v-navigation-drawer
-          style="z-index:2"
+          style="z-index:2; max-height: 100%"
           clipped
           permanent
           mini-variant
           mini-variant-width="5vw"
+          height="100%"
       >
 
         <div class="icon_sidebar_container">
@@ -52,45 +53,46 @@
         </div>
       </v-navigation-drawer>
 
-      <!-- Assessment and factory sidebar -->
+      <!-- Assessment and factory sidebar (second) -->
       <v-navigation-drawer
-          style="z-index:1; background-color: #F2F4F3"
+          style="z-index:1; background-color: #F2F4F3;"
           v-model="secondMenu"
           :width="secondMenu ? '15vw' : '0vw'"
           flat
+
       >
 
-        <div style="display: flex; justify-content: flex-end; padding: 7px ">
-          <v-icon @click="secondMenu = !secondMenu">mdi-close</v-icon>
+        <div style=" height: 100%; position: fixed; width: 100%">
+          <div style="display: flex; justify-content: flex-end; padding: 7px ">
+            <v-icon @click="secondMenu = !secondMenu">mdi-close</v-icon>
+          </div>
+
+          <h1>Assessment list</h1>
+          <div style = "overflow-y: auto; height: 85%; max-height: 85%; width: 100%">
+            <v-expansion-panels>
+              <v-expansion-panel
+                  v-for="(assessment, assessment_index ) in created_assessments"
+                  :key="assessment.name"
+              >
+                <v-expansion-panel-header disable-icon-rotate>
+                  {{ assessment.name }}
+                  <template v-slot:actions>
+                    <v-icon color="teal" @click="rightMenu = true; right_sidebar_content = 2; selected_assessment = assessment_index" @click.native.stop>
+                      mdi-map
+                    </v-icon>
+                  </template>
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  Lorem ipsum dolor sit amet, conssdffffffffff fffffffffffffffff fffffffffffffffffff fffffffffff ffffffffff ffff ffffff fffffffffff fffffff ffffecte tur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </div>
+          <v-btn block @click="rightMenu = true; right_sidebar_content = 1">
+            Create assessment
+          </v-btn>
         </div>
 
-        <h1>Assessment list</h1>
-
-          <v-list>
-            <v-list-item-group v-model="asdf">
-              <v-list-item
-                  v-for="(assessment, index ) in created_assessments"
-                  :key="index"
-              >
-                  {{assessment.name}}
-
-                <br>
-                <v-list>
-                  <v-list-item
-                      v-for="(industry, index_ind ) in assessment.industries"
-                      :key="industry.name"
-                  >
-                    {{industry.name}}
-                  </v-list-item>
-                </v-list>
-              </v-list-item>
-            </v-list-item-group>
-
-          </v-list>
-
-        <v-btn block @click="rightMenu = !rightMenu">
-          Create assessment
-        </v-btn>
       </v-navigation-drawer>
 
       <!-- Main content -->
@@ -110,7 +112,7 @@
         <div style="padding: 7px ">
           <v-icon @click="rightMenu = !rightMenu">mdi-close</v-icon>
         </div>
-        <div>
+        <div v-if="right_sidebar_content === 1">
           <h1>Create assessment</h1>
           <v-form
               ref="create_assessment_ref"
@@ -128,9 +130,73 @@
             </v-btn>
           </v-form>
         </div>
+        <div v-if="right_sidebar_content === 2">
+
+          <v-tabs
+              v-model="tab"
+              background-color="deep-purple accent-4"
+
+              dark
+          >
+            <v-tabs-slider></v-tabs-slider>
+
+            <v-tab href="#tab-1">
+              <v-icon>mdi-phone</v-icon>
+            </v-tab>
+
+            <v-tab href="#tab-2">
+              <v-icon>mdi-heart</v-icon>
+            </v-tab>
+
+
+          </v-tabs>
+
+          <v-tabs-items v-model="tab">
+            <v-tab-item
+                v-for="i in 3"
+                :key="i"
+                :value="'tab-' + i"
+            >
+              <v-card flat>
+                <v-card-text>adsfsadf</v-card-text>
+              </v-card>
+            </v-tab-item>
+          </v-tabs-items>
+
+          <v-text-field
+              v-model="created_assessments[selected_assessment].name"
+              :rules="[new_assessment_rules.name, new_assessment_rules.required]"
+
+          ></v-text-field>
+          <v-btn>
+            Edit
+          </v-btn>
+          <v-btn>
+            Add factory
+          </v-btn>
+          <v-btn>
+            Delete
+          </v-btn>
+        </div>
       </v-navigation-drawer>
 
     </v-layout>
+    <v-snackbar
+        v-model="new_assessment_snackbar"
+        :timeout="2000"
+    >
+      New assessment created correctly
+      <template v-slot:action="{ attrs }">
+        <v-btn
+            color="pink"
+            text
+            v-bind="attrs"
+            @click="new_assessment_snackbar = false"
+        >
+          CLOSE
+        </v-btn>
+      </template>
+    </v-snackbar>
 
   </v-app>
 </template>
@@ -153,7 +219,7 @@ export default {
       ],
       created_assessments: this.$assessments,  //Created assessments
       assessment_name: null,     //Default name for creating an assessment
-      new_assessment_rules: {
+      new_assessment_rules: { //Rules for creating new assessment
         name: value => {  //Assessment name must be unique
           let assessments_with_same_name = this.created_assessments.filter(assessment => {
             return assessment.name === value
@@ -162,7 +228,11 @@ export default {
         },
         required: value => !!value || 'Required.',
       },
-      new_assessment_valid: false,
+      new_assessment_valid: false,  //Enable or disable button for creating new assessment
+      new_assessment_snackbar: false, //Snackbar when a new assessment is created
+      right_sidebar_content: null,  //Content of the right sidebar: 1->create assessment, 2->assessment information
+      selected_assessment: null,
+      tab: null
     }
   },
   methods: {
@@ -177,6 +247,12 @@ export default {
       this.$assessments.push(new_assessment)
       this.rightMenu = !this.rightMenu
       this.assessment_name = null
+      this.new_assessment_snackbar = true
+    },
+    open_right_sidebar_show_assessment(){
+      this.right_sidebar_content = 2
+      this.secondMenu = true
+
     }
   }
 }
@@ -189,6 +265,7 @@ html {
   overflow: hidden !important;
   scrollbar-width: none;
   -ms-overflow-style: none;
+  width: 100%;
 }
 
 html::-webkit-scrollbar {
@@ -244,6 +321,10 @@ html::-webkit-scrollbar {
 
 .assessment_list{
   background-color: red;
+}
+
+#app{
+  width: 100%;
 }
 
 </style>
