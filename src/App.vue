841c46c9b2efd6b9666bcca4c5bbd14afb3de82b
zephol <joansalograu@gmail.com>
2021-07-08 12:30:42 +0200

    management of industries

--- src/App.vue
+++ src/App.vue
@@ -77,18 +77,24 @@
                 <v-expansion-panel-header disable-icon-rotate>
                   {{ assessment.name }}
                   <template v-slot:actions>
-                    <v-icon color="teal" @click="rightMenu = true; right_sidebar_content = 2; selected_assessment = assessment_index" @click.native.stop>
-                      mdi-map
+                    <v-icon color="teal" @click="open_edit_assessment_tab(assessment_index)" @click.native.stop>
+                      mdi-cog
                     </v-icon>
                   </template>
                 </v-expansion-panel-header>
                 <v-expansion-panel-content>
-                  Lorem ipsum dolor sit amet, conssdffffffffff fffffffffffffffff fffffffffffffffffff fffffffffff ffffffffff ffff ffffff fffffffffff fffffff ffffecte tur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  <div
+                    v-for="(industry,industry_index ) in created_assessments[assessment_index].industries"
+                    :key="industry.name"
+                  >
+                    {{industry.name}}
+                    <v-icon @click = "open_edit_industry_tab(assessment_index, industry_index)">mdi-circle-edit-outline</v-icon>
+                  </div>
                 </v-expansion-panel-content>
               </v-expansion-panel>
             </v-expansion-panels>
           </div>
-          <v-btn block @click="rightMenu = true; right_sidebar_content = 1">
+          <v-btn block @click="rightMenu = true; right_sidebar_content = 1; assessment_name = null">
             Create assessment
           </v-btn>
         </div>
@@ -102,7 +108,7 @@
         </div>
       </v-main>
 
-      <!-- Assessment/Company creation sidebar -->
+      <!--right menu -->
       <v-navigation-drawer
           v-model="rightMenu"
           style="background-color: #F2F4F3"
@@ -112,6 +118,7 @@
         <div style="padding: 7px ">
           <v-icon @click="rightMenu = !rightMenu">mdi-close</v-icon>
         </div>
+        <!-- Assessment creation -->
         <div v-if="right_sidebar_content === 1">
           <h1>Create assessment</h1>
           <v-form
@@ -130,57 +137,108 @@
             </v-btn>
           </v-form>
         </div>
-        <div v-if="right_sidebar_content === 2">
+        <!-- Edit settings -->
+        <div v-else-if="right_sidebar_content === 2">
 
           <v-tabs
-              v-model="tab"
+              v-model="edit_assessment_tab"
               background-color="deep-purple accent-4"
-
               dark
           >
             <v-tabs-slider></v-tabs-slider>
 
-            <v-tab href="#tab-1">
-              <v-icon>mdi-phone</v-icon>
+            <v-tab href="#tab-edit">
+              <v-icon>mdi-circle-edit-outline</v-icon>
             </v-tab>
 
-            <v-tab href="#tab-2">
-              <v-icon>mdi-heart</v-icon>
+            <v-tab href="#tab-add-factory">
+              <v-icon>mdi-factory</v-icon>
             </v-tab>
 
 
           </v-tabs>
 
-          <v-tabs-items v-model="tab">
+          <v-tabs-items v-model="edit_assessment_tab">
             <v-tab-item
-                v-for="i in 3"
-                :key="i"
-                :value="'tab-' + i"
+                key="edit"
+                value="tab-edit"
             >
               <v-card flat>
-                <v-card-text>adsfsadf</v-card-text>
+                <v-form
+                    v-model="new_assessment_valid"
+                >
+                  <v-text-field
+                      v-model="assessment_name"
+                      :rules="[edit_assessment_rules.name, edit_assessment_rules.required]"
+
+                  ></v-text-field>
+                  <v-btn
+                      :disabled="!new_assessment_valid"
+                      @click="edit_assessment"
+
+                  >
+                    Edit
+                  </v-btn>
+                </v-form>
+                <v-btn @click = "delete_assessment">
+                  Delete
+                </v-btn>
+              </v-card>
+            </v-tab-item>
+            <v-tab-item
+                key="add-factory"
+                value="tab-add-factory"
+            >
+              <v-card flat>
+                <v-form
+                    v-model="new_factory_valid"
+                >
+                  <v-text-field
+                      v-model="factory_name"
+                      label="Industry name"
+                      :rules="[new_factory_rules.name, new_factory_rules.required]"
+
+                  ></v-text-field>
+                  <v-btn
+                      :disabled="!new_factory_valid"
+                      @click="add_factory"
+
+                  >
+                    Add industry
+                  </v-btn>
+                </v-form>
+
               </v-card>
             </v-tab-item>
           </v-tabs-items>
-
-          <v-text-field
-              v-model="created_assessments[selected_assessment].name"
-              :rules="[new_assessment_rules.name, new_assessment_rules.required]"
-
-          ></v-text-field>
+        </div>
+        <div v-else-if="right_sidebar_content === 3">
+          <h1>Edit industry</h1>
+          <v-form
+              v-model="new_factory_valid"
+          >
+            <v-text-field
+                v-model="factory_name"
+                :rules="[edit_industry_rules.name, edit_industry_rules.required]"
+            ></v-text-field>
+            <v-btn
+                :disabled="!new_factory_valid"
+                @click="edit_industry">
+              Edit industry
+            </v-btn>
+          </v-form>
           <v-btn>
-            Edit
+            Advanced edit
           </v-btn>
-          <v-btn>
-            Add factory
-          </v-btn>
-          <v-btn>
+          <v-btn @click="delete_industry">
             Delete
           </v-btn>
         </div>
+
       </v-navigation-drawer>
 
     </v-layout>
+    <!-- Snackbar for creating assessment -->
     <v-snackbar
         v-model="new_assessment_snackbar"
         :timeout="2000"
@@ -192,6 +250,96 @@
             text
             v-bind="attrs"
             @click="new_assessment_snackbar = false"
+        >
+          CLOSE
+        </v-btn>
+      </template>
+    </v-snackbar>
+
+    <!-- Snackbar for editing assessment -->
+    <v-snackbar
+        v-model="edit_assessment_snackbar"
+        :timeout="2000"
+    >
+      Assessment edited correctly
+      <template v-slot:action="{ attrs }">
+        <v-btn
+            color="pink"
+            text
+            v-bind="attrs"
+            @click="edit_assessment_snackbar = false"
+        >
+          CLOSE
+        </v-btn>
+      </template>
+    </v-snackbar>
+
+    <!-- Snackbar for deleting assessment -->
+    <v-snackbar
+        v-model="delete_assessment_snackbar"
+        :timeout="2000"
+    >
+      Assessment deleted correctly
+      <template v-slot:action="{ attrs }">
+        <v-btn
+            color="pink"
+            text
+            v-bind="attrs"
+            @click="delete_assessment_snackbar = false"
+        >
+          CLOSE
+        </v-btn>
+      </template>
+    </v-snackbar>
+
+    <!-- Snackbar for  creating new industry-->
+    <v-snackbar
+        v-model="create_industry_snackbar"
+        :timeout="2000"
+    >
+      New industry added correctly
+      <template v-slot:action="{ attrs }">
+        <v-btn
+            color="pink"
+            text
+            v-bind="attrs"
+            @click="create_industry_snackbar = false"
+        >
+          CLOSE
+        </v-btn>
+      </template>
+    </v-snackbar>
+
+    <!-- Snackbar for  editing industry-->
+    <v-snackbar
+        v-model="edit_industry_snackbar"
+        :timeout="2000"
+    >
+      Industry edited correctly
+      <template v-slot:action="{ attrs }">
+        <v-btn
+            color="pink"
+            text
+            v-bind="attrs"
+            @click="edit_industry_snackbar = false"
+        >
+          CLOSE
+        </v-btn>
+      </template>
+    </v-snackbar>
+
+    <!-- Snackbar for  deleting industry-->
+    <v-snackbar
+        v-model="delete_industry_snackbar"
+        :timeout="2000"
+    >
+      Industry deleted correctly
+      <template v-slot:action="{ attrs }">
+        <v-btn
+            color="pink"
+            text
+            v-bind="attrs"
+            @click="delete_industry_snackbar = false"
         >
           CLOSE
         </v-btn>
@@ -218,7 +366,7 @@ export default {
         { title: "Show statistics", icon: 'mdi-chart-areaspline', to:"map" },
       ],
       created_assessments: this.$assessments,  //Created assessments
-      assessment_name: null,     //Default name for creating an assessment
+      assessment_name: null,     //V-model name for creating/editing an assessment
       new_assessment_rules: { //Rules for creating new assessment
         name: value => {  //Assessment name must be unique
           let assessments_with_same_name = this.created_assessments.filter(assessment => {
@@ -228,11 +376,47 @@ export default {
         },
         required: value => !!value || 'Required.',
       },
+      edit_assessment_rules: { //Rules for editing assessment
+        name: value => {  //Assessment name must be unique
+          let assessments_with_same_name = this.created_assessments.filter(assessment => {
+            return assessment.name === value
+          })
+          return (assessments_with_same_name.length === 0 || (assessments_with_same_name.length === 1 && this.created_assessments[this.selected_assessment].name === value)) || 'An assessment with same name already exists.' //If there is an assessment with the same name, must be the edited assessment
+        },
+        required: value => !!value || 'Required.',
+      },
       new_assessment_valid: false,  //Enable or disable button for creating new assessment
       new_assessment_snackbar: false, //Snackbar when a new assessment is created
-      right_sidebar_content: null,  //Content of the right sidebar: 1->create assessment, 2->assessment information
-      selected_assessment: null,
-      tab: null
+      right_sidebar_content: null,  //Content of the right sidebar: 1->create assessment, 2->edit assessment
+      selected_assessment: null,  //Id of the assessment to edit
+      edit_assessment_tab: 0, //Vmodel for edit assessment tab
+      edit_assessment_snackbar: false, //Snackbar for when an assessment is edited
+      delete_assessment_snackbar: false, //Snackbar for when an assessment is deleted
+      factory_name: null, //v-model for creating new factory
+      new_factory_rules: { //Rules for creating new factory
+        name: value => {  // Factory name must bu unique inside an assessment
+          let factories_with_same_name = this.created_assessments[this.selected_assessment].industries.filter(company => {
+            return company.name === value
+          })
+          return factories_with_same_name.length === 0 || 'An industry with same name already exists. '
+        },
+        required: value => !!value || 'Required.',
+      },
+      new_factory_valid: false, //Enable or disable button for creating new factory
+      create_industry_snackbar: false, //Snackbar for when new industry added
+      edit_industry_rules: { //Rules for editing industry
+        name: value => {  //Industry name must be unique
+          let industries_with_same_name = this.created_assessments[this.selected_assessment].industries.filter(industry => {
+            return industry.name === value
+          })
+          return (industries_with_same_name.length === 0 || (industries_with_same_name.length === 1 && this.created_assessments[this.selected_assessment].industries[this.selected_industry].name === value)) || 'An assessment with same name already exists.' //If there is an assessment with the same name, must be the edited assessment
+        },
+        required: value => !!value || 'Required.',
+      },
+      selected_industry: null, //Id of the company to edit
+      edit_industry_snackbar: false, //Snackbar for when editing industry
+      delete_industry_snackbar: false, //Snackbar for when deleting industry
+
     }
   },
   methods: {
@@ -249,12 +433,52 @@ export default {
       this.assessment_name = null
       this.new_assessment_snackbar = true
     },
-    open_right_sidebar_show_assessment(){
-      this.right_sidebar_content = 2
-      this.secondMenu = true
+    open_edit_assessment_tab(assessment_index){
+      this.rightMenu = true;
+      this.right_sidebar_content = 2;
+      this.selected_assessment = assessment_index
+      this.assessment_name = this.created_assessments[assessment_index].name
+      this.edit_assessment_tab = 0
+      this.factory_name = null
 
+    },
+    edit_assessment(){
+      this.rightMenu = false
+      this.$assessments[this.selected_assessment].name = this.assessment_name
+      this.edit_assessment_snackbar = true
+    },
+    delete_assessment(){
+      this.rightMenu = false
+      this.delete_assessment_snackbar = true
+      this.$assessments.splice(this.selected_assessment, 1)
+    },
+    add_factory(){
+      let industry = new Industry()
+      industry.name = this.factory_name
+      let assessment = this.$assessments[this.selected_assessment]
+      assessment.add_industry(industry)
+      this.rightMenu = false
+      this.factory_name = null
+      this.create_industry_snackbar = true
+    },
+    open_edit_industry_tab(assessment_index, industry_index){
+      this.right_sidebar_content = 3
+      this.rightMenu = true
+      this.factory_name = this.created_assessments[assessment_index].industries[industry_index].name
+      this.selected_assessment = assessment_index
+      this.selected_industry = industry_index
+    },
+    edit_industry(){
+      this.rightMenu = false
+      this.$assessments[this.selected_assessment].industries[this.selected_industry].name = this.factory_name
+      this.edit_industry_snackbar = true
+    },
+    delete_industry(){
+      this.rightMenu = false
+      this.delete_industry_snackbar = true
+      this.$assessments[this.selected_assessment].delete_industry(this.selected_industry)
     }
-  }
+  },
 }
 </script>
 
--- src/ecam_backend.js
+++ src/ecam_backend.js
@@ -51,6 +51,7 @@ export class Assessment{
     delete_industry(i){
         this.industries.splice(i,1);
     }
+
 };
 
 /*
