2aa94cd0806da3521b61e11f9da6b94cab64ed69
Joan Saló <joansalograu@gmail.com>
2023-03-14 10:22:14 +0100

    no cache configuration

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 
   <!-- Disable cache so that the user can always see the latest version of the app -->
-  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+  <meta http-equiv="Cache-Control" content="no-cache, must-revalidate" />
   <meta http-equiv="Pragma" content="no-cache" />
   <meta http-equiv="Expires" content="0" />
 
