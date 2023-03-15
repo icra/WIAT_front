b86dcb8fc6cc4005a22d5bf316f1b7cd721ef7e0
Joan Saló <joansalograu@gmail.com>
2023-03-15 15:04:32 +0100

    no cache configuration

--- src/App.vue
+++ src/App.vue
@@ -37,7 +37,6 @@
               <div style="background-color: white; height: 100%; width: 100%">
 
                 <v-card flat style="padding: 40px; height: 100%;">
-                  <b>This video is from the older version of the WIAT tool. Some things has changed in this version, but the way assessments and industries are added is the same.</b>
                   <br><br>
                   <video controls id="video" style="padding: 40px; width: 70%">
                     <source src="demo.mp4" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
